e26e94ed72801de32b57a96b481a57866d72a417
hutonahill <eriker@byui.edu>
2023-11-02 22:15:08 -0600

    made number_guess more testable

--- number_guess.py
+++ number_guess.py
@@ -8,26 +8,20 @@ def subtract_turn(turn):
     newTurn = turn-1
     return newTurn
 
-print()
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-print()
-
-# Number of attempts allowed
-attemptsLeft = 10
-# Number of attempts completed
-attempts = 1
-# Array holds the user's previous guesses
-guesses = []
-
-while attemptsLeft > 0:
+def runAttempt(guesses:list, attemptsLeft:int, testing:bool, userGuess:int = 10):
     # Print out the number of tries left
     print(f"Attempts left: {attemptsLeft}")
+    
     # If there are guesses in the array, then print the array
     if len(guesses) > 0:
         print(f"Previous Guesses: {guesses}")
+    
     #user enters guess here
-    userGuess = int(input("Enter a number: "))
+    
+    if(testing == False):
+        userGuess = int(input("Enter a number: "))
+    
+    
     #Add the user guess to the guesses array
     guesses.append(userGuess)
 
@@ -40,6 +34,7 @@ while attemptsLeft > 0:
         attemptsLeft = subtract_turn(attemptsLeft)
         # Add 1 to the number of attempts
         attempts += 1
+    
     # Guess too high
     elif userGuess > ranNum:
         print()
@@ -49,6 +44,7 @@ while attemptsLeft > 0:
         attemptsLeft = subtract_turn(attemptsLeft)
         # Add 1 to the number of attempts
         attempts += 1
+    
     else:
         print()
         # Prints the correct answer
@@ -56,24 +52,45 @@ while attemptsLeft > 0:
         print()
         # Turns attemptsLeft into a negative number so that it exits the while loop on line 23
         attemptsLeft = -1
+    
+    return (attemptsLeft, guesses)
 
-# If the player runs out of turns
-if attemptsLeft == 0:
-    print("GAME OVER. You've run out of turns.")
-    print(f"The number was {ranNum}")
+def main(testing:bool, attemptsLeft:int = 10):
+
     print()
-    print("GAME SUMMARY: ")
-    print(f"Total number of attempts: {attempts}")
-    print(f"Guesses: {guesses}")
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
     print()
-# If the player wins
-elif attemptsLeft == -1:
-    print("Thank you for playing! ")
-    print()
-    print("GAME SUMMARY: ")
-    print(f"Total number of attempts: {attempts}")
-    print(f"Guesses: {guesses}")
-    print()
+
+    # Number of attempts allowed
+    attemptsLeft
+    
+    # Number of attempts completed
+    attempts = 1
+    
+    # Array holds the user's previous guesses
+    guesses = []
+
+    while attemptsLeft > 0:
+        (attemptsLeft, guesses) = runAttempt(guesses, attemptsLeft, testing)
+
+    # If the player runs out of turns
+    if attemptsLeft == 0:
+        print("GAME OVER. You've run out of turns.")
+        print(f"The number was {ranNum}")
+        print()
+        print("GAME SUMMARY: ")
+        print(f"Total number of attempts: {attempts}")
+        print(f"Guesses: {guesses}")
+        print()
+    # If the player wins
+    elif attemptsLeft == -1:
+        print("Thank you for playing! ")
+        print()
+        print("GAME SUMMARY: ")
+        print(f"Total number of attempts: {attempts}")
+        print(f"Guesses: {guesses}")
+        print()
 
 # # Generate a random number between 1 and 100
 # secret_number = random.randint(1, 100)
@@ -98,3 +115,7 @@ elif attemptsLeft == -1:
 
 #     except ValueError:
 #         print("Please enter a valid number.")
+
+
+if (__name__ == "__main__"):
+    main()
